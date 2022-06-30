e3d3ca366f4c77943356b2c6d47867af0aa0b46b
amit.kumar <amit.kumar@galaxycard.in>
2022-06-30 13:05:21 +0530

    Fixes headers

--- android/src/main/java/in/galaxycard/android/utils/TurboStarterPackage.kt
+++ android/src/main/java/in/galaxycard/android/utils/TurboStarterPackage.kt
@@ -1,10 +1,9 @@
 package `in`.galaxycard.android.utils
 
 import com.facebook.react.ReactPackage
-import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
-import java.util.ArrayList
 
 class TurboStarterPackage : ReactPackage {
     override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
