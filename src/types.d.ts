388e14d9e144ad172f4481eea31b0088dbe2642b
amit.kumar <amit.kumar@galaxycard.in>
2022-07-07 16:28:50 +0530

    export constants

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,37 @@
+export interface Constants {
+  uniqueId: string;
+  deviceId: string;
+  bundleId: string;
+  systemVersion: string;
+  appVersion: string;
+  buildNumber: string;
+  appName: string;
+  brand: string;
+  model: string;
+  screenWidth: number;
+  screenHeight: number;
+  screenDensity: number;
+  installReferrer: string;
+}
+
+export interface DeviceData {
+  hasHeadphones: boolean;
+  carrier: string;
+  airplaneMode: boolean;
+  batteryState: string;
+  batteryLevel: number;
+  lowPowerMode: boolean;
+  pinOrFingerprintSet: boolean;
+  fontScale: number;
+  freeDiskStorage: number;
+  totalDiskCapacity: number;
+  maxMemory: number;
+  usedMemory: number;
+  hasLocation: boolean;
+  wifiName: string;
+  accessPointName: string;
+}
+
 export interface Contact {
   name?: string;
   phones?: string[];
