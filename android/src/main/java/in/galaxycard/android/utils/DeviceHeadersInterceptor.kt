2eb96b3ba6da8425ed7e070174890ec95d6cba29
amit.kumar <amit.kumar@galaxycard.in>
2022-07-24 19:31:56 +0530

    Allows unicode in headers

--- android/src/main/java/in/galaxycard/android/utils/DeviceHeadersInterceptor.kt
+++ android/src/main/java/in/galaxycard/android/utils/DeviceHeadersInterceptor.kt
@@ -5,6 +5,7 @@ import android.content.pm.PackageManager
 import android.os.Build
 import android.util.Base64
 import androidx.annotation.NonNull
+import okhttp3.Headers
 import okhttp3.Interceptor
 import okhttp3.Response
 import java.io.IOException
@@ -40,47 +41,51 @@ class DeviceHeadersInterceptor(private val context: Context): Interceptor {
     @Throws(IOException::class)
     override fun intercept(chain: Interceptor.Chain): Response {
         val builder = chain.request().newBuilder()
+        val headers = Headers.Builder()
         if (mSignatureHash != null) {
-            builder.addHeader("x-gct-signature", mSignatureHash!!)
+            headers.add("x-gct-signature", mSignatureHash!!)
         }
         val constants = DeviceUtils(context).constants()
         if (chain.request().header("device") == null) {
-            builder.addHeader("device", constants["uniqueId"]!! as String)
+            headers.add("device", constants["uniqueId"]!! as String)
+        } else {
+            headers.add("device", chain.request().header("device")!!)
         }
-        builder.addHeader("build", constants["buildNumber"]!! as String)
-        builder.addHeader("build-number", constants["buildNumber"]!! as String)
-        builder.addHeader("app-version", constants["appVersion"]!! as String)
-        builder.addHeader("os-version", constants["systemVersion"]!! as String)
-        builder.addHeader("device-code", constants["deviceId"]!! as String)
-        builder.addHeader("brand", constants["brand"]!! as String)
-        builder.addHeader("model", constants["model"]!! as String)
-        builder.addHeader("install-referrer", constants["installReferrer"]!! as String)
-        builder.addHeader("screen-width", constants["screenWidth"]!!.toString())
-        builder.addHeader("screen-height", constants["screenHeight"]!!.toString())
-        builder.addHeader("screen-density", constants["screenDensity"]!!.toString())
+        headers.add("build", constants["buildNumber"]!! as String)
+        headers.add("build-number", constants["buildNumber"]!! as String)
+        headers.add("app-version", constants["appVersion"]!! as String)
+        headers.add("os-version", constants["systemVersion"]!! as String)
+        headers.add("device-code", constants["deviceId"]!! as String)
+        headers.add("brand", constants["brand"]!! as String)
+        headers.add("model", constants["model"]!! as String)
+        headers.addUnsafeNonAscii("install-referrer", constants["installReferrer"]!! as String)
+        headers.addUnsafeNonAscii("x-gct-install-referrer", constants["installReferrer"]!! as String)
+        headers.add("screen-width", constants["screenWidth"]!!.toString())
+        headers.add("screen-height", constants["screenHeight"]!!.toString())
+        headers.add("screen-density", constants["screenDensity"]!!.toString())
 
         val deviceData = DeviceUtils(context).dynamicValues()
-        builder.addHeader("carrier", deviceData["carrier"] as String)
+        headers.addUnsafeNonAscii("carrier", deviceData["carrier"] as String)
         val headphones = if (deviceData["hasHeadphones"] as Boolean) "yes" else "no"
-        builder.addHeader("headphones-connected", headphones)
+        headers.add("headphones-connected", headphones)
         val airplaneMode = if (deviceData["airplaneMode"] as Boolean) "on" else "off"
-        builder.addHeader("airplane-mode", airplaneMode)
-        builder.addHeader("battery-level", deviceData[DeviceUtils.BATTERY_LEVEL].toString())
-        builder.addHeader("battery-state", deviceData[DeviceUtils.BATTERY_STATE] as String)
+        headers.add("airplane-mode", airplaneMode)
+        headers.add("battery-level", deviceData[DeviceUtils.BATTERY_LEVEL].toString())
+        headers.add("battery-state", deviceData[DeviceUtils.BATTERY_STATE] as String)
         val lowPowerMode = if (deviceData[DeviceUtils.LOW_POWER_MODE] as Boolean) "on" else "off"
-        builder.addHeader("low-power-mode", lowPowerMode)
-        builder.addHeader("font-scale", deviceData["fontScale"].toString())
-        builder.addHeader("disk-total", (deviceData["totalDiskCapacity"] as BigDecimal).toPlainString())
-        builder.addHeader("disk-available", (deviceData["freeDiskStorage"] as BigDecimal).toPlainString())
-        builder.addHeader("memory-total", (deviceData["maxMemory"] as BigDecimal).toPlainString())
-        builder.addHeader("memory-used", (deviceData["usedMemory"] as BigDecimal).toPlainString())
+        headers.add("low-power-mode", lowPowerMode)
+        headers.add("font-scale", deviceData["fontScale"].toString())
+        headers.add("disk-total", (deviceData["totalDiskCapacity"] as BigDecimal).toPlainString())
+        headers.add("disk-available", (deviceData["freeDiskStorage"] as BigDecimal).toPlainString())
+        headers.add("memory-total", (deviceData["maxMemory"] as BigDecimal).toPlainString())
+        headers.add("memory-used", (deviceData["usedMemory"] as BigDecimal).toPlainString())
         val pinOrFingerprintSet = if (deviceData["pinOrFingerprintSet"] as Boolean) "yes" else "no"
-        builder.addHeader("pin-or-fingerprint-set", pinOrFingerprintSet)
+        headers.add("pin-or-fingerprint-set", pinOrFingerprintSet)
         val locationEnabled = if (deviceData["hasLocation"] as Boolean) "yes" else "no"
-        builder.addHeader("location-enabled", locationEnabled)
-        builder.addHeader("wifi-name", deviceData["wifiName"] as String)
-        builder.addHeader("access-point-name", deviceData["accessPointName"] as String)
-        builder.addHeader("device-name", deviceData["deviceName"] as String)
+        headers.add("location-enabled", locationEnabled)
+        headers.addUnsafeNonAscii("wifi-name", deviceData["wifiName"] as String)
+        headers.addUnsafeNonAscii("access-point-name", deviceData["accessPointName"] as String)
+        headers.addUnsafeNonAscii("device-name", deviceData["deviceName"] as String)
 
         return chain.proceed(builder.build())
     }
