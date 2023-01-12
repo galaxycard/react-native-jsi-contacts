b0bb6415a98956610946d88a67a2dbae402a29df
GCT <samgalaxycard>
2023-01-12 13:51:47 +0530

    feat: update package.json

--- android/src/main/java/in/galaxycard/android/utils/ReactNativeTurboUtilsModule.kt
+++ android/src/main/java/in/galaxycard/android/utils/ReactNativeTurboUtilsModule.kt
@@ -180,18 +180,14 @@ class ReactNativeTurboUtilsModule() : Module() {
         OnCreate {
             DeviceUtils(appContext.reactContext!!)
             val filter = IntentFilter()
-//            filter.addAction(Intent.ACTION_BATTERY_CHANGED)
             filter.addAction(Intent.ACTION_POWER_CONNECTED)
             filter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
             filter.addAction(Intent.ACTION_POWER_DISCONNECTED)
             filter.addAction(PowerManager.ACTION_POWER_SAVE_MODE_CHANGED)
-//            filter.addAction(AudioManager.ACTION_HEADSET_PLUG)
-//            filter.addAction(AudioManager.ACTION_SCO_AUDIO_STATE_UPDATED)
             filter.addAction(LocationManager.PROVIDERS_CHANGED_ACTION)
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 filter.addAction(CarrierConfigManager.ACTION_CARRIER_CONFIG_CHANGED)
             }
-//            filter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION)
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
                 filter.addAction(TelephonyManager.ACTION_SUBSCRIPTION_CARRIER_IDENTITY_CHANGED)
             }
--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,22 @@
-
 import ReactNativeTurboUtilsModule from "./ReactNativeTurboUtilsModule";
-import { PermissionResponse } from 'expo-modules-core';
-
-interface InstalledApps {
-    apps: {
-      system: boolean;
-      name: string;
-      package: string;
-      install: number;
-      update: number;
-    }[];
-    hash: string;
-}
-
-type Contacts = {
-    contacts: {
-        id: number;
-        name?: string;
-        phones?: string[];
-        emails?: string[];
-        photo?: string;
-    }[],
-    hash: string;
-}
+import { Contacts, InstalledApps } from "./types";
 
 export function getDeviceData() {
-    return ReactNativeTurboUtilsModule.getDeviceData();
+  return ReactNativeTurboUtilsModule.getDeviceData();
 }
 
 export function getConstants() {
-    return ReactNativeTurboUtilsModule.getConstants();
+  return ReactNativeTurboUtilsModule.getConstants();
 }
 
 export async function getInstalledApps(): Promise<InstalledApps> {
-    return await ReactNativeTurboUtilsModule.getInstalledApps();
+  return await ReactNativeTurboUtilsModule.getInstalledApps();
 }
 
 export async function getContacts(): Promise<Contacts> {
-    return ReactNativeTurboUtilsModule.getContacts();
+  return ReactNativeTurboUtilsModule.getContacts();
 }
 
 export function parseJwt(jwt: string, key: string | null) {
-    return ReactNativeTurboUtilsModule.parseJwt(jwt, key);
-}
-
-export async function testDummy(): Promise<String> {
-    return ReactNativeTurboUtilsModule.testDummy();
+  return ReactNativeTurboUtilsModule.parseJwt(jwt, key);
 }
