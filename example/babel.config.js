82624790cb2c262ebd10da5c6fc8cdaa59a4a80e
GCT <samgalaxycard>
2023-01-13 18:00:52 +0530

    fix: testinggg

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,16 +1,22 @@
-const path = require('path');
+const path = require("path");
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
     plugins: [
       [
-        'module-resolver',
+        "module-resolver",
         {
-          extensions: ['.tsx', '.ts', '.js', '.json'],
+          extensions: [".tsx", ".ts", ".js", ".json"],
           alias: {
             // For development, we want to alias the library to the source
-            'react-native-turbo-utils': path.join(__dirname, '..', 'src', 'index.ts'),
+            "react-native-turbo-utils": path.join(
+              // eslint-disable-next-line no-undef
+              __dirname,
+              "..",
+              "src",
+              "index.ts"
+            ),
           },
         },
       ],
