83b1ab15298893fa87f9c6b9ec3303042d47009f
amit.kumar <amit.kumar@galaxycard.in>
2023-06-01 16:24:23 +0530

    fix: add types in web

--- src/index.web.ts
+++ src/index.web.ts
@@ -1,1 +1,4 @@
 // This is a no-op file, as we do not have any role on the web
+import { Contact, DeviceInfoType, InstalledApps } from "./types";
+
+export { Contact, InstalledApps, DeviceInfoType };
