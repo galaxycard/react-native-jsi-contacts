ce8d368f38423643fc457fe7f3c7e8dd8d3a92ae
GCT <samgalaxycard>
2023-06-02 00:09:16 +0530

    fix: seperate ReactNativeTurboUtilsModule from index and seperate for web and native

--- src/index.web.ts
+++
@@ -1,4 +0,0 @@
-// This is a no-op file, as we do not have any role on the web
-import { Contact, DeviceInfoType, InstalledApps } from "./types";
-
-export { Contact, InstalledApps, DeviceInfoType };
