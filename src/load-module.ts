451e1d5362fcc1d49e5f44ee01cfd93108db12bd
amit.kumar <amit.kumar@galaxycard.in>
2023-06-14 16:29:08 +0530

    fix: onDeviceListener

--- src/load-module.ts
+++ src/load-module.ts
@@ -1,3 +1,14 @@
+import { EventEmitter, Subscription } from "expo-modules-core";
+
 import ReactNativeTurboUtilsModule from "./ReactNativeTurboUtilsModule";
+import { DeviceInfoType } from "./types";
+
+const emitter = new EventEmitter(ReactNativeTurboUtilsModule);
+
+export function addDeviceInfoChangedListener(
+  listener: (event: DeviceInfoType) => void
+): Subscription {
+  return emitter.addListener("onDeviceInfoChanged", listener);
+}
 
 export default ReactNativeTurboUtilsModule;
